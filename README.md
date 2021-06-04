b16d166801cf70aa8140c5e96a60a981034b5d95
HarisGkn <haris.gjo@gmail.com>
2021-06-04 13:05:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ergasia_2_E16028_Gkiontsari_Charilaos
+ Uni Project
---
+++ app.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from flask import Flask, request, jsonify, redirect, Response
+from bson import json_util
+import json
+import uuid
+import time
+
+# Connect to our local MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+
+# Choose database
+db = client['DSMarkets']
+
+# Choose collections
+users = db['Users']
+products = db['Products']
+
+# Initiate Flask App
+app = Flask(__name__)
+
+users_sessions = {}
+
+def create_session(username):
+    user_uuid = str(uuid.uuid1())
+    users_sessions[user_uuid] = (username, time.time())
+    return user_uuid  
+
+def is_session_valid(user_uuid):
+    # return user_uuid in users_sessions
+    return uuids.find()
+
+# Εκτέλεση flask service σε debug mode, στην port 5000. 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
